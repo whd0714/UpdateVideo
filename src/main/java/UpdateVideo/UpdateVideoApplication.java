52869b6b6237545f5bd84b71f96262e4d4938253
whd07 <jonghyun0714@gmail.com>
2021-07-01 15:54:54 +0900

    Initial commit

---
+++ src/main/java/UpdateVideo/UpdateVideoApplication.java
@@ -0,0 +1,13 @@
+package UpdateVideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdateVideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpdateVideoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/UpdateVideo/UpdateVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package UpdateVideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdateVideoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
